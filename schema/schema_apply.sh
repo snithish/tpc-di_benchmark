fc487d61378637d8448685b3ab2c654efdca6043
Nithish Sankaranarayanan <nithishsankaranarayanan@gmail.com>
2019-11-04 19:56:13 +0100

    Script to create and destroy datasets

--- schema/schema_apply.sh
+++ schema/schema_apply.sh
@@ -5,7 +5,7 @@
 STG_DS=staging
 MASTER_DS=master
 
-bq rm -f $STG_DS && bq mk $STG_DS
-bq rm -f $MASTER_DS && bq mk $MASTER_DS
+bq rm -f -r $STG_DS && bq mk $STG_DS
+bq rm -f -r $MASTER_DS && bq mk $MASTER_DS
 
 cat schema/staging/create_table.sql | bq query --use_legacy_sql=false --batch=false
