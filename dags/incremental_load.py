e11bebc98d20d2c5638578e888444af288740dec
Nithish Sankaranarayanan <nithishsankaranarayanan@gmail.com>
2019-11-13 18:15:58 +0100

    import cash transaction table to staging

--- dags/incremental_load.py
+++ dags/incremental_load.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 from airflow import DAG
+
 from utils import construct_gcs_to_bq_operator, get_file_path
 
 default_args = {
@@ -12,7 +13,7 @@ default_args = {
     'retries': 0
 }
 
-with DAG('load_account', schedule_interval=None, default_args=default_args) as dag:
+with DAG('incremental_load', schedule_interval=None, default_args=default_args) as dag:
     load_account_file_to_staging = construct_gcs_to_bq_operator('load_account_to_staging',
                                                                 get_file_path(True, 'Account'), [
                                                                     {"name": "CDC_FLAG", "type": "STRING",
@@ -31,3 +32,18 @@ with DAG('load_account', schedule_interval=None, default_args=default_args) as d
                                                                      "mode": "REQUIRED"},
                                                                     {"name": "CA_ST_ID", "type": "STRING",
                                                                      "mode": "REQUIRED"}], 'staging.account')
+    load_cash_transaction_file_to_staging = construct_gcs_to_bq_operator('load_cash_transaction_to_staging',
+                                                                         get_file_path(True, 'CashTransaction'), [
+                                                                             {"name": "CDC_FLAG", "type": "STRING",
+                                                                              "mode": "REQUIRED"},
+                                                                             {"name": "CDC_DSN", "type": "INTEGER",
+                                                                              "mode": "REQUIRED"},
+                                                                             {"name": "CT_CA_ID", "type": "INTEGER",
+                                                                              "mode": "REQUIRED"},
+                                                                             {"name": "CT_DTS", "type": "DATETIME",
+                                                                              "mode": "REQUIRED"},
+                                                                             {"name": "CT_AMT", "type": "FLOAT",
+                                                                              "mode": "REQUIRED"},
+                                                                             {"name": "CT_NAME", "type": "STRING",
+                                                                              "mode": "REQUIRED"}],
+                                                                         'staging.cash_transaction')
