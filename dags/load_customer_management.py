f503833760029938943a236cb332c861facdbae5
Nithish Sankaranarayanan <nithishsankaranarayanan@gmail.com>
2019-11-19 20:36:43 +0100

    Load prospect to staging

--- dags/load_customer_management.py
+++ dags/load_customer_management.py
@@ -3,7 +3,8 @@ from datetime import datetime
 from airflow import DAG
 from airflow.contrib.operators.gcs_to_bq import GoogleCloudStorageToBigQueryOperator
 
-from constants import GCS_BUCKET, BIG_QUERY_CONN_ID, GOOGLE_CLOUD_DEFAULT
+from constants import GCS_BUCKET, BIG_QUERY_CONN_ID, GOOGLE_CLOUD_DEFAULT, CSV_EXTENSION
+from utils import construct_gcs_to_bq_operator, get_file_path
 
 AIRFLOW = 'airflow'
 
@@ -29,3 +30,52 @@ with DAG('load_customer_account', schedule_interval=None, default_args=default_a
         google_cloud_storage_conn_id=GOOGLE_CLOUD_DEFAULT,
         ignore_unknown_values=False
     )
+
+    load_prospect_file_to_staging = construct_gcs_to_bq_operator('load_prospect_historical_to_staging',
+                                                                 get_file_path(False, 'Prospect', CSV_EXTENSION), [
+                                                                     {"name": "AgencyID", "type": "STRING",
+                                                                      "mode": "REQUIRED"},
+                                                                     {"name": "LastName", "type": "STRING",
+                                                                      "mode": "REQUIRED"},
+                                                                     {"name": "FirstName", "type": "STRING",
+                                                                      "mode": "REQUIRED"},
+                                                                     {"name": "MiddleInitial", "type": "STRING",
+                                                                      "mode": "NULLABLE"},
+                                                                     {"name": "Gender", "type": "STRING",
+                                                                      "mode": "NULLABLE"},
+                                                                     {"name": "AddressLine1", "type": "STRING",
+                                                                      "mode": "NULLABLE"},
+                                                                     {"name": "AddressLine2", "type": "STRING",
+                                                                      "mode": "NULLABLE"},
+                                                                     {"name": "PostalCode", "type": "STRING",
+                                                                      "mode": "NULLABLE"},
+                                                                     {"name": "City", "type": "STRING",
+                                                                      "mode": "REQUIRED"},
+                                                                     {"name": "State", "type": "STRING",
+                                                                      "mode": "REQUIRED"},
+                                                                     {"name": "Country", "type": "STRING",
+                                                                      "mode": "NULLABLE"},
+                                                                     {"name": "Phone", "type": "STRING",
+                                                                      "mode": "NULLABLE"},
+                                                                     {"name": "Income", "type": "INTEGER",
+                                                                      "mode": "NULLABLE"},
+                                                                     {"name": "NumberCars", "type": "INTEGER",
+                                                                      "mode": "NULLABLE"},
+                                                                     {"name": "NumberChildren", "type": "INTEGER",
+                                                                      "mode": "NULLABLE"},
+                                                                     {"name": "MaritalStatus", "type": "STRING",
+                                                                      "mode": "NULLABLE"},
+                                                                     {"name": "Age", "type": "INTEGER",
+                                                                      "mode": "NULLABLE"},
+                                                                     {"name": "CreditRating", "type": "INTEGER",
+                                                                      "mode": "NULLABLE"},
+                                                                     {"name": "OwnOrRentFlag", "type": "STRING",
+                                                                      "mode": "NULLABLE"},
+                                                                     {"name": "Employer", "type": "STRING",
+                                                                      "mode": "NULLABLE"},
+                                                                     {"name": "NumberCreditCards", "type": "INTEGER",
+                                                                      "mode": "NULLABLE"},
+                                                                     {"name": "NetWorth", "type": "INTEGER",
+                                                                      "mode": "NULLABLE"}], 'staging.prospect')
+
+    [load_customer_management_staging, load_prospect_file_to_staging]
